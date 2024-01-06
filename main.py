fdc5be2a7a9b3e5cdfa742b2c36d1f06707e800e
Ewan <jmewan35@gmail.com>
2024-01-06 22:04:14 +0000

    cleaned up file and added comments

--- main.py
+++ main.py
@@ -10,12 +10,11 @@ faithful_data = pd.read_csv("faithful.csv")
 faithful_data = faithful_data.drop("rownames", axis = 1)
 faithful_data = faithful_data.dropna()
 
+# writing introduction and adding graph
 st.write("This will be a basic analysis and visualisation of the faithful data set, built in with R.")
 st.write("This data set contains 2 columns, one for the duration of a volcano's last eruption, and another for the " +  
          "length of time between eruptions. A plot of these variables can be found below.")
 
-
-eruption_time = faithful_data["eruptions"]
-waiting_time = faithful_data["waiting"]
-
 st.scatter_chart(faithful_data, x = "eruptions", y = "waiting")
+
+
